c348fcb85d7d6373faef9248de8f7765d7df016f
hhyyg <hiroka.yago@gmail.com>
2017-12-05 16:54:20 +0900

    不要なものを削除

--- Miso.TrackingMealApp/TrackingMealApp/MealTableViewController.swift
+++ Miso.TrackingMealApp/TrackingMealApp/MealTableViewController.swift
@@ -120,21 +120,16 @@ class MealTableViewController: UITableViewController {
         }
     }
 
-    // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
         return true
     }
 
-    // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            // Delete the row from the data source
             meals.remove(at: indexPath.row)
             try! saveMeals()
             tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }
     }
 
